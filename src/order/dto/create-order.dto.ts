43641df8dbb0c7823aa83ad4a010d586e5f12763
trendeltrep <maksy.krykukha@nure.ua>
2023-12-01 20:04:43 +0200

    fix order

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,9 +1,5 @@
-import { OrderStatus } from "src/constants/enum";
 
 export class CreateOrderDto {
-    totalWaitTime: number;
-    status:OrderStatus;
-    totalPrice:number;
     tip:number;
     customerId:string;
     waiterId:string;
